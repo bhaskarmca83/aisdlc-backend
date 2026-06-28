055b46371f76fd920f70f3582d6bc085922a1dba
bhaskarmca83 <58994212+bhaskarmca83@users.noreply.github.com>
2026-06-27 21:11:04 -0500

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/aisdlc/AisdlcApplication.java
@@ -0,0 +1,11 @@
+package com.aisdlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisdlcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AisdlcApplication.class, args);
+    }
+}
